9f7d577d48bc25298ca421da46c57b400c7ccb84
Unknown <ibrahim.a.hamid@gmail.com>
2018-12-12 23:19:10 +0200

    Added 'color' prop

--- README.md
+++ README.md
@@ -16,6 +16,21 @@ Show this component in loading state (loading another component or waiting for d
 <LoadingSpinner/>
 ```
 
+You can pass a color prop to control the color of the spinner.
+
+```js
+<LoadingSpinner color="blue"/>
+<LoadingSpinner color="red"/>
+<LoadingSpinner color="#20a8d8"/>
+```
+
+## Options
+
+#### `color`
+
+The color of the spinner.
+
+
 ## License
 
 MIT
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './index.css';
+
 class LoadingSpinner extends React.Component {
     render() {
+        const color = this.props.color ? this.props.color : '#20a8d8';
+        const divStyle = {
+            position: 'absolute',
+            border: '4px solid ' + color,
+            opacity: '1',
+            borderRadius: '50%',
+            animation: 'lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite'
+        };
         return (
             <div className="lds-ripple">
-                <div></div>
-                <div></div>
+                <div style={divStyle}></div>
+                <div style={divStyle}></div>
             </div>
         );
     }
 }
+
 export default LoadingSpinner;
